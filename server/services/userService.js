f06538eb68b9f4a972a5a6056f1182a44991f9b2
Sheff2000 <sheffpb@gmail.com>
2024-01-08 15:13:42 +0200

    init user service

---
+++ server/services/userService.js
@@ -0,0 +1,19 @@
+// логика работы с юзерами - создание/обновление
+import User from '../models/modelUser.js';
+
+const createUser = async (userData) => {
+
+};
+
+const updatePasswordUser = async (passData) => {
+
+};
+
+const getUserById = async (userId) => {
+
+};
+
+export { createUser, 
+         getUserById,
+         updatePasswordUser,
+};
