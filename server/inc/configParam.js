244d0fc10298c88befb99b1cfa790054bfb4aad2
Sheff2000 <sheffpb@gmail.com>
2024-01-11 09:33:35 +0200

    delete readonly config

--- server/inc/configParam.js
+++ server/inc/configParam.js
@@ -3,8 +3,6 @@ import 'dotenv/config';
 export const configDB = {
     user:           process.env.DB_USER,
     password:       process.env.DB_PASSWORD,
-    userRead:       process.env.DB_USER_READ,
-    passwordRead:   process.env.DB_PASSWORD_READ,
     host:           process.env.DB_HOST,
     database:       process.env.DB_BASE_NAME,
     collection:     process.env.DB_COLLECTION_NAME,
