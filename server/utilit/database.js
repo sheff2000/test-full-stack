a1dafed22f9a4a6721bffa8060c680aeb45dc9c1
Sheff2000 <sheffpb@gmail.com>
2024-01-11 09:34:31 +0200

    remove read only connect

--- server/utilit/database.js
+++ server/utilit/database.js
@@ -4,9 +4,4 @@ import mongoose from 'mongoose';
 export const connectReadWrite = () => {
     const uri = `mongodb+srv://${configDB.user}:${configDB.password}@${configDB.host}`;
     return mongoose.connect(uri, { dbName: configDB.database });
-  };
-  
-  export const connectReadOnly = () => {
-    const uri = `mongodb+srv://${configDB.userRead}:${configDB.passwordRead}@${configDB.host}`;
-    return mongoose.connect(uri, { dbName: configDB.database });
-  };
+};
