e208f25ccab322b12848ad8e15150514ca6d571a
Sheff2000 <sheffpb@gmail.com>
2024-01-11 09:33:20 +0200

    add connect to mongo in start app

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import {connectReadWrite} from './utilit/database.js';
 import { serverConfig } from './inc/configParam.js';
 
 const app = express();
 const port = serverConfig.port || 3002 ;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+connectReadWrite().then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+}).catch(err => {
+    console.error('Database connection failed', err);
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
