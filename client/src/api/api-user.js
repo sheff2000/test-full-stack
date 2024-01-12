01b37de29c0d9c56d30f43e16cac5f33a285bd92
Sheff2000 <sheffpb@gmail.com>
2024-01-12 11:48:34 +0200

    add api service to user routes

---
+++ client/src/api/api-user.js
@@ -0,0 +1,28 @@
+const BASE_SERVER_URL = 'http://localhost';
+
+async function post(url, method, data) {
+  try {
+    const response = await fetch(`${BASE_SERVER_URL}${url}`, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    if (response.ok) {
+      return response.json();
+    } else {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'виникла помилка');
+    }
+  } catch (error) {
+    console.error('Помилка в запросі', error);
+    throw error;
+  }
+}
+
+export const api = {
+  login: (data) => post('/user/login', 'POST', data),
+  register: (data) => post('/user/register', 'POST', data),
+  // Другие API-запросы...
+};
