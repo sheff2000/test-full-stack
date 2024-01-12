98f433c431af8e97e19ea61f2cdf98b45a108170
Sheff2000 <sheffpb@gmail.com>
2024-01-12 13:49:03 +0200

    add routes groups

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import {connectReadWrite} from './utilit/database.js';
 import { serverConfig } from './inc/configParam.js';
+import userRoutes from './routes/userRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+
 
 const app = express();
 const port = serverConfig.port || 3002 ;
+
+app.use(express.json());
+
+app.use('/user', userRoutes);
+app.use('/project', projectRoutes);
+app.use('/task', taskRoutes);
 
 connectReadWrite().then(() => {
     console.log('Connected to MongoDB');
