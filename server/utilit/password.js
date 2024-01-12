adbbefa206b9fd9addf6b86eb3ed48cc24710a5d
Sheff2000 <sheffpb@gmail.com>
2024-01-12 14:04:25 +0200

    add password utilit

---
+++ server/utilit/password.js
@@ -0,0 +1,22 @@
+// работа с паролями
+import bcrypt from 'bcrypt';
+
+export const comparePassword = async (password, hash) => {
+    try {
+      return await bcrypt.compare(password, hash);
+    } catch (error) {
+      console.error('Error comparing password and hash:', error);
+      throw new Error(`Error comparing password and hash: ${error}`)
+    }
+  };
+
+export const generatePasswordHash = async (password) => {
+    const saltRounds = 10; // Количество раундов генерации соли
+    try {
+      const hash = await bcrypt.hash(password, saltRounds);
+      return hash;
+    } catch (error) {
+      console.error('Error generating password hash:', error);
+      throw new Error(`Error generating password hash: ${error}`)
+    }
+  };
