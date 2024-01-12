9fb1905589b54a1b3b1e08778680cf3c9f3da8b3
Sheff2000 <sheffpb@gmail.com>
2024-01-12 23:17:24 +0200

    fix bugs

---
+++ server/routes/projectRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const router = express.Router();
+
+//router.get('/projects', userController.login);
+//router.put('/updatepassword/:userId', verifyToken, updateUserPassword);
+
+export default router;
--- server/utilit/errorOperation.js
+++ server/utilit/errorOperation.js
@@ -1,8 +1,7 @@
-const errorOperation = (err, req, res, next) => {
+export const errorOperation = (err, req, res, next) => {
     console.error(err); 
     // тут надо придумать логгирование - в файл или монгу?? 
     // err.toString();
     res.status(err.status || 500).json({ message: err.message || "Internal server error" });
 };
 
-export default errorWork;
