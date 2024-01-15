962ddab93af884d14c0179eedcdb6937377e7808
Sheff2000 <sheffpb@gmail.com>
2024-01-15 15:58:44 +0200

    add create user service

--- server/services/userService.js
+++ server/services/userService.js
@@ -10,6 +10,10 @@ const loginUser = async (userData) => {
         if (!user) {
             // не нашли юзера - отправляем 401 код
             const error = new Error(`Користувача з імʼям ${userName} не знайдено`);
+            console.log('---------------------');
+            console.log(error);
+            console.log('----------------------');
+            error.debug = `Error in findUserbyUserName(userData.userName). userName - ${userData.userName}. Not finde USER`;
             error.status = 401;
             throw(error);
         }
@@ -17,6 +21,7 @@ const loginUser = async (userData) => {
         const isPasswordOk = await password.comparePassword(userData.userPassword, user.passwordUser);
         if (!isPasswordOk) {
             const error = new Error(`Invalid password`);
+            error.debug = `Error in userService / login User. Password not compare`;
             error.status = 401;
             throw(error);
         }
@@ -33,21 +38,43 @@ const loginUser = async (userData) => {
         }
         return {userInfo, token}
     } catch(err) {
-        throw err;
+        const error = new Error(err.message || `Internal server error`);
+        error.debug = `Error catch in userService / login User. stack err - ${err}`;
+        error.status = err.status || 500;
+        throw (error);
     }
 }
 
 const createUser = async (userData) => {
     try {
-        console.log('USER SERVISE. userData - ', userData);
+        //console.log('USER SERVISE. userData - ', userData);
+        const isExistUser = await userModel.findUserbyUserName(userData.userName);
+        if (isExistUser) {
+            const error = new Error(`Користувача з імʼям ${userData.userName} вже існує`);
+            error.status = 422;
+            throw(error);
+        }
+
+        userData.userPassword = await password.generatePasswordHash(userData.userPassword);
         return await userModel.createUser(userData);
     } catch(err) {
-        throw err;
+        const error = new Error(err.message || `Internal server error`);
+        error.debug = `Error catch in userService / Create User. msg err - ${err}`;
+        error.status = err.status || 500;
+        throw (error);
     }
 };
 
 const updatePasswordUser = async (passData) => {
-
+    try {
+        console.log('updatePasswordUser - ', passData);
+        
+    } catch(err) {
+        const error = new Error(err.message || `Internal server error`);
+        error.debug = `Error catch in userService / updatePasswordUser. stack err - ${err}`;
+        error.status = err.status || 500;
+        throw (error);
+    }
 };
 
 const getUserById = async (userId) => {
