33f9a58b9dca588dbf5c8d50993f1ca52abb21e0
Sheff2000 <sheffpb@gmail.com>
2024-01-15 14:05:36 +0200

    add user params validation

--- server/validation/userValidation.js
+++ server/validation/userValidation.js
@@ -1,4 +1,19 @@
 // валидация данных для юзера
+import { validDataConfig } from "../inc/configParam.js";
+
+const validUserName =  (userName) => {
+    if (!userName || userName.length === 0) {
+        return false;
+    }
+    return validDataConfig.userName.characterSet.test(userName)
+};
+
+const validUserPassword =  (userPassword) => {
+    if (!userPassword || userPassword.length < validDataConfig.userPassword.minLen ) {
+        return false;
+    }
+    return true;
+};
 
 const validUserDataToUserCreate = async (userData) => {
         // проверка наличия всех нужных полей и соответствие типов
@@ -6,6 +21,10 @@ const validUserDataToUserCreate = async (userData) => {
         return {err:false, msg: ''}
 };
 
-export {
+const validator = {
+    validUserName,
+    validUserPassword,
     validUserDataToUserCreate,
-}
+};
+
+export default validator;
