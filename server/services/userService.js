7fcd59614348577763c4d429dd45dc5ba145e36f
Sheff2000 <sheffpb@gmail.com>
2024-01-15 14:05:17 +0200

    add user service

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,10 +1,46 @@
 // логика работы с юзерами - создание/обновление
-import User from '../models/modelUser.js';
-import { validUserDataToUserCreate } from '../validation/userValidation.js';
+import userModel from "../models/modelUser.js";
+import password from "../utilit/password.js";
+import tokenUtil from "../utilit/token.js";
+
+const loginUser = async (userData) => {
+    try {
+        // find by name
+        const user = await userModel.findUserbyUserName(userData.userName);
+        if (!user) {
+            // не нашли юзера - отправляем 401 код
+            const error = new Error(`Користувача з імʼям ${userName} не знайдено`);
+            error.status = 401;
+            throw(error);
+        }
+        // compare password
+        const isPasswordOk = await password.comparePassword(userData.userPassword, user.passwordUser);
+        if (!isPasswordOk) {
+            const error = new Error(`Invalid password`);
+            error.status = 401;
+            throw(error);
+        }
+        const token = tokenUtil.generate({userId: user._id, userName: user.nameUser});
+
+        // get count project and tasks for this user
+
+        // data to return
+        const userInfo = {
+            userId: user._id, 
+            userName: user.nameUser,
+            countProjects: 0,
+            countTasks: 0,
+        }
+        return {userInfo, token}
+    } catch(err) {
+        throw err;
+    }
+}
 
 const createUser = async (userData) => {
     try {
-        const resultValid = await validUserData(userData); // валидация на наличе всех нужніх данніх
+        console.log('USER SERVISE. userData - ', userData);
+        return await userModel.createUser(userData);
     } catch(err) {
         throw err;
     }
@@ -18,7 +54,8 @@ const getUserById = async (userId) => {
 
 };
 
-export { createUser, 
-         getUserById,
-         updatePasswordUser,
+export {    loginUser,
+            createUser, 
+            getUserById,
+            updatePasswordUser,
 };
