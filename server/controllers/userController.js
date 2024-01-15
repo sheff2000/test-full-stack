ae7a02581a811c5644a8bb3cf96f48c6b39a30ff
Sheff2000 <sheffpb@gmail.com>
2024-01-15 16:01:15 +0200

    add register controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,51 +1,57 @@
-import tokenUtil from "../utilit/token.js";
-import userModel from "../models/modelUser.js";
-import password from "../utilit/password.js";
+import validator from "../validation/userValidation.js";
+import {loginUser, createUser} from "../services/userService.js";
 
-const login = async (req, res) => {
-    const { userName, userPassword } = req.body;
+const login = async (req, res, next) => {
     try {
-        // по идее тут надо вызвать функции валидации данных ?!?!?!
-
-        // find by name
-        const user = await userModel.findUserbyUserName(userName);
-        if (!user) {
-            // не нашли юзера - отправляем 401 код
-            // а это профессиональный подход? 
-            
-            const error = new Error(`Користувача з імʼям ${userName} не знайдено`);
+        console.log('in login controller');
+        // определить все входные аргументы
+        const userData = { userName: req.body.login, userPassword: req.body.password };
+        // валидация - наличие всех нужных данных, проверка данных на корректность
+        if (!validator.validUserName(userData.userName) && !validator.validUserPassword(userData.userPassword)) {
+            const error = new Error(`Не корректний формат логіну та/або паролю`);
             error.status = 401;
             return next(error);
         }
-        // compare password
-        const isPasswordOk = await password.comparePassword(userPassword, user.passwordUser);
-        if (!isPasswordOk) {
-            const error = new Error(`Invalid password`);
-            error.status = 401;
-            return next(error);
-        }
-        // data to return
-        const userInfo = {
-            userId: user._id, 
-            userName: user.nameUser,
-            countProjects: 0,
-            countTasks: 0,
-        }
+        // проверка прав доступа на текущий роутер - тут не надо
 
-        const token = tokenUtil.generate({userId: user._id, userName: user.nameUser});
-        return res.json({ token, userInfo });
+        // передаем все на сервис 
+        // обробатываем ошибки / делаем передачу данных
+        const result = await loginUser(userData);
+        return res.json(result);
     } catch(err) {
         console.error(err);
-        const error = new Error("Internal server error");
-        error.status = 500;
+        const error = new Error(err.message || "Internal server error");
+        error.status = error.status || 500;
         return next(error);
     }
 };
 
 
-const register = async (req, res) => {
-    result = {token, userData}
-    return res.json(result);
+const register = async (req, res, next) => {
+    try {
+        // определить все входные аргументы
+        const userData = { userName: req.body.login, userPassword: req.body.password };
+        //console.log('FRON SEND USER DATA - ', userData);
+        // валидация - наличие всех нужных данных, проверка данных на корректность
+        if (!validator.validUserName(userData.userName) && !validator.validUserPassword(userData.userPassword)) {
+            const error = new Error(`Не корректний формат логіну та/або паролю`);
+            error.debug = `Error validation validUserName and validUserPassword userData - ${userData}`;
+            error.status = 401;
+            return next(error);
+        }
+        // проверка прав доступа на текущий роутер - тут не надо
+
+        // передаем все на сервис 
+        const result = await createUser(userData);
+
+        return res.json(result);
+    } catch(err) {
+        console.error(err);
+        const error = new Error(err.message || "Internal server error");
+        error.debug = err.debug || `Error in user controller / register. stack - ${err}`;
+        error.status = error.status || 500;
+        return next(error);
+    }
 };
 
 const userController = {
