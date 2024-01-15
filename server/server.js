070292a8aa84752b80cef7024b9ab8418f6b2149
Sheff2000 <sheffpb@gmail.com>
2024-01-15 14:04:07 +0200

    init cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import {connectReadWrite} from './utilit/database.js';
 import { serverConfig } from './inc/configParam.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,11 +11,17 @@ import {errorOperation} from './utilit/errorOperation.js';
 const app = express();
 const port = serverConfig.port || 3002 ;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/user', userRoutes);
 app.use('/project', projectRoutes);
 app.use('/task', taskRoutes);
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.use(errorOperation);
 
