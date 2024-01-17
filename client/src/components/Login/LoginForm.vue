8bab7e6af120fb294bffddcf40c5863b3b0a16c5
Sheff2000 <sheffpb@gmail.com>
2024-01-17 13:11:18 +0200

    add valid login data in form

--- client/src/components/Login/LoginForm.vue
+++ client/src/components/Login/LoginForm.vue
@@ -1,24 +1,66 @@
 <script setup>
+
+import { ref, computed } from 'vue';
+import useUserStore from '@/stores/userStore';
+
+const userStore = useUserStore();
+const loginAuth = ref('');
+const passwordAuth = ref('');
+
+const validLoginAuth = ref('');
+const validPasswordAuth = ref('');
+
+const validLogin = () => {
+  validLoginAuth.value = userStore.loginRgp.test(loginAuth.value);
+};
+const validPassword = () => {
+  validPasswordAuth.value = passwordAuth.value.length > 5;
+};
+
+const passwordInputClasses = computed(() => {
+  if (validPasswordAuth.value === '') { // лдя начального вывода поля формы без стилей валидации
+    return {}; // Возвращает пустой объект, если validPasswordRegister пусто
+  }
+  return {
+    'is-valid': validPasswordAuth.value,
+    'is-invalid': !validPasswordAuth.value,
+  };
+});
+const loginInputClasses = computed(() => {
+  if (validLoginAuth.value === '') {
+    return {};
+  }
+  return {
+    'is-valid': validLoginAuth.value,
+    'is-invalid': !validLoginAuth.value,
+  };
+});
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="">
     <div class="mb-3">
-      <label for="login-username" class="form-label">Логин</label>
+      <label for="login-username" class="form-label">Логін</label>
       <input type="text"
         class="form-control"
+        :class="loginInputClasses"
         id="login-username"
-        placeholder="Введите ваш логин"
+        placeholder="Введіть ваш логін"
+        v-model="loginAuth"
+        @input="validLogin"
         required>
     </div>
     <div class="mb-3">
       <label for="login-password" class="form-label">Пароль</label>
       <input type="password"
         class="form-control"
+        :class="passwordInputClasses"
         id="login-password"
-        placeholder="Введите ваш пароль"
+        placeholder="Введіть ваш пароль"
+        v-model="passwordAuth"
+        @input="validPassword"
         required>
     </div>
-    <button type="button" class="btn btn-primary">Авторизация</button>
+    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Авторизація</button>
   </form>
 </template>
