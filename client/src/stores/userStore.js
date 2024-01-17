64cf64ebb97b96c7334d1343a3fd334e419d57db
Sheff2000 <sheffpb@gmail.com>
2024-01-17 12:51:38 +0200

    add valid Rgp by login

--- client/src/stores/userStore.js
+++ client/src/stores/userStore.js
@@ -3,11 +3,21 @@ import { defineStore } from 'pinia';
 const useUserStore = defineStore('user', {
   state: () => ({
     showModal: false,
+    loginRgp: /^[A-Za-z0-9_-]+$/,
   }),
   actions: {
     toggleModal() {
       this.showModal = !this.showModal;
       console.log('toggle model - ', this.showModal);
+    },
+    register() {
+
+    },
+    login() {
+
+    },
+    logout() {
+
     },
   },
 });
