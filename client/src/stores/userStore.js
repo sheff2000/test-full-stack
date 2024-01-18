9960a212bdbd85a694eafcaf5bd1465b0b3c2a49
Sheff2000 <sheffpb@gmail.com>
2024-01-18 09:50:11 +0200

    init register func

--- client/src/stores/userStore.js
+++ client/src/stores/userStore.js
@@ -1,25 +1,36 @@
 import { defineStore } from 'pinia';
+import userApiController from '@/api/userApiController'; 
 
 const useUserStore = defineStore('user', {
-  state: () => ({
-    showModal: false,
-    loginRgp: /^[A-Za-z0-9_-]+$/,
-  }),
-  actions: {
-    toggleModal() {
-      this.showModal = !this.showModal;
-      console.log('toggle model - ', this.showModal);
-    },
-    register() {
+    state: () => ({
+        showModal: false,
+        loginRgp: /^[A-Za-z0-9_-]+$/,
+    }),
 
-    },
-    login() {
+    actions: {
+        toggleModal() {
+            this.showModal = !this.showModal;
+            console.log('toggle model - ', this.showModal);
+        },
+        register(registerData) {
+          /* 
+          * Регистрация юзера - отправка данных на сервер
+          * @param {object} registerData
+          * @param {string} registerData.loginRegister
+          * @param {string} registerData.passwordRegister
+          * 
+          * 1 - validation register data
+          * 2 - send to api controller
+          * 3 - reciev result from answer
+          */
+        },
+        login() {
 
-    },
-    logout() {
+        },
+        logout() {
 
+        },
     },
-  },
 });
 
 export default useUserStore;
