609eaf2e1b1865de89d1d744f5b253547f5bef27
Sheff2000 <sheffpb@gmail.com>
2024-01-18 09:50:00 +0200

    create main api func

---
+++ client/src/api/apiProjectServer.js
@@ -0,0 +1,27 @@
+const BASE_SERVER_URL = 'http://localhost:3002';
+
+async function sendReq(url, method, headers, data) {
+    try {
+        console.log('API - url ', url, ' | method - ', method, ' | data - ', data);
+        const response = await fetch(`${BASE_SERVER_URL}${url}`, {
+            method: method,
+            headers: headers,
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            return response.json();
+        } else {
+            const errorData = await response.json();
+            throw new Error(errorData.message || `виникла помилка error - ${errorData}`);
+        }
+    } catch (error) {
+            console.error('Помилка в запросі', error);
+            throw error;
+    }
+};
+
+const apiProjectServer = {
+    sendReq,
+};
+
+export default apiProjectServer;
